ac30ee07abd721d41f1409d5f7b804d473e95615
esherape <andrew.bazilskiy@gmail.com>
2019-08-29 17:36:20 +0300

    error indicator, loader

--- src/components/random-planet/random-planet.js
+++ src/components/random-planet/random-planet.js
@@ -1,14 +1,20 @@
 import React, {Component} from 'react';
 import SwapiService from "../../services/swapi-service";
+import Spinner from "../spinner";
+import ErrorIndicator from "../error-indicator";
 
 import './random-planet.css';
+import PlanetView from "./planet-view";
+
 
 export default class RandomPlanet extends Component {
 
   swapiService = new SwapiService();
 
   state = {
-    planet: {}
+    planet: {},
+    loading: true,
+    error: false
   };
 
   constructor() {
@@ -17,38 +23,41 @@ export default class RandomPlanet extends Component {
   }
 
   onPlanetLoaded = (planet) => {
-    this.setState({planet});
+    this.setState({
+      planet,
+      loading: false
+    });
+  };
+
+  onError = (err) => {
+    this.setState({
+      error: true,
+      loading: false
+    });
   };
 
   updatePlanet() {
     const id = Math.floor(Math.random() * 25 + 2);
-    this.swapiService.getPlanet(id).then(this.onPlanetLoaded)
+    this.swapiService
+      .getPlanet(id)
+      .then(this.onPlanetLoaded)
+      .catch(this.onError);
   }
 
   render() {
-    const {planet: {id, name, population, rotationPeriod, diameter}} = this.state;
+    const {planet, loading, error} = this.state;
+
+    const hasData = !(loading || error);
+    const errorMessage = error ? <ErrorIndicator/> : null;
+    const spinner = loading ? <Spinner/> : null;
+    const content = hasData ? <PlanetView planet={planet}/> : null;
+
     return (
       <div className='col-12'>
         <div className="random-planet jumbotron rounded">
-          <img alt='planet' className="planet-image"
-               src={`https://starwars-visualguide.com/assets/img/planets/${id}.jpg`}/>
-          <div>
-            <h4>{name}</h4>
-            <ul className="list-group list-group-flush ml-0">
-              <li className="list-group-item">
-                <span className="term">Population:</span>
-                <span>{population}</span>
-              </li>
-              <li className="list-group-item">
-                <span className="term">Rotation Period:</span>
-                <span>{rotationPeriod}</span>
-              </li>
-              <li className="list-group-item">
-                <span className="term">Diameter:</span>
-                <span>{diameter}</span>
-              </li>
-            </ul>
-          </div>
+          {errorMessage}
+          {spinner}
+          {content}
         </div>
       </div>
     );
