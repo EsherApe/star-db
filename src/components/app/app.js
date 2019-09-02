7f30e059c90a9abcd2ecfab8ebcc433a3b384a5a
esherape <andrew.bazilskiy@gmail.com>
2019-09-02 19:03:38 +0300

    page refactoring

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -2,13 +2,12 @@ import React, {Component} from 'react';
 import {SwapiServiceProvider} from "../swapi-service-context";
 import SwapiService from "../../services/swapi-service";
 import DummySwapiService from "../../services/dummy-swapi-service";
+import './app.css';
 
 import Header from '../header';
 import RandomPlanet from '../random-planet';
-import PeoplePage from "../people-page";
-
-import './app.css';
 import ErrorBoundary from "../error-boundary";
+import {PeoplePage, PlanetsPage, StarshipsPage} from '../pages'
 
 class App extends Component {
 
@@ -34,6 +33,8 @@ class App extends Component {
           <Header onServiceChange={this.onServiceChange}/>
           <RandomPlanet/>
           <PeoplePage/>
+          <PlanetsPage/>
+          <StarshipsPage/>
         </SwapiServiceProvider>
       </ErrorBoundary>
     );
